906af5c693c686c070d39c17a13faafb7d6b2825
deepmbhatt <deepmanishbhatt@gmail.com>
2023-12-29 23:42:33 +0530

    Tip Calculator Code

--- day2tipcalculator.py
+++ day2tipcalculator.py
@@ -1,4 +1,4 @@
-#This is a tip calculator which helps you find the amount of bill a person has to pay while going out in a group
+#This is a tip calculator which helps you find the amount of bill a person has to pay while going out in a group.
 print("Wecome to the tip calculator")
 bill=int(input("What was the total bill?\n"))
 tip_percent=int(input("What percentage tip would you like to give? 10, 12, or 15?\n"))
