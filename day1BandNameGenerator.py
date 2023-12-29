a900f0b07658018c07921dc633488cad88b35580
deepmbhatt <deepmanishbhatt@gmail.com>
2023-12-29 23:41:33 +0530

    Band Name Generator Code

--- day1BandNameGenerator.py
+++ day1BandNameGenerator.py
@@ -1,3 +1,4 @@
+#This is a code which generates a band name based on the city you grew up in and the name of your pet
 print("Welcome to the Band Name Generator.")
 city=input("What's the name of the city you grew up in?\n")
 pet=input("What is the name of a pet?\n")
