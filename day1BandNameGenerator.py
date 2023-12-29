15bdf129c99b4f7a29f21c92630a04961c76d02d
deepmbhatt <deepmanishbhatt@gmail.com>
2023-12-29 23:00:13 +0530

    Day 1 band generator code final

--- day1BandNameGenerator.py
+++ day1BandNameGenerator.py
@@ -1,1 +1,4 @@
-print("hello")
+print("Welcome to the Band Name Generator.")
+city=input("What's the name of the city you grew up in?\n")
+pet=input("What is the name of a pet?\n")
+print("Your band name could be "+city+" "+pet)
