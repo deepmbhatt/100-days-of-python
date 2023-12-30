bfa3f26dd2bc4e8d747bb4be1868db922212a74d
deepmbhatt <deepmanishbhatt@gmail.com>
2023-12-30 17:40:33 +0530

    Password Generator Code

--- day5Passwordgenerator.py
+++ day5Passwordgenerator.py
@@ -13,12 +13,12 @@ for i in range(98,123):
     letters.append(chr(i))
 numbers=['0','1','2','3','4','5','6','7','8','9']
 symbols=['!','@','#','$','%','&','*','(',')','+']
-password=""
+password=[]
 for i in range(n_let):
-    password+=letters[random.randint(0,len(letters)-1)]
+    password+=random.choice(letters)
 for i in range(n_sym):
-    password+=symbols[random.randint(0,len(symbols)-1)]
+    password+=random.choice(symbols)
 for i in range(n_num):
-    password+=numbers[random.randint(0,len(numbers)-1)]
+    password+=random.choice(numbers)
 password=''.join(random.sample(password,len(password)))
 print(password)
