2affbd2c2951b1c4b2f9c189bf2bb6417926fdb6
deepmbhatt <deepmanishbhatt@gmail.com>
2024-01-18 18:12:30 +0530

    Oddeven Leap year FIZZBIZZ

---
+++ day13Debugging/debugfizzbuzz.py
@@ -0,0 +1,11 @@
+#Here if the entered value is divisible by 3 it prints Fizz or if by 5 then BUzz if both then FizzBuzz
+target = int(input())
+for number in range(1, target + 1):
+  if number % 3 == 0 and number % 5 == 0:
+    print("FizzBuzz")
+  elif number % 3 == 0:
+    print("Fizz")
+  elif number % 5 == 0:
+    print("Buzz")
+  else:
+    print(number)
